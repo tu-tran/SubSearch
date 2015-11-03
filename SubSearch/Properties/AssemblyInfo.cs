93d928107ccccbdb340ed3fb1ff49e20efff6d03
Tu Tran <tu.tran@tekla.com>
2015-11-03 15:55:36 +0200

    Fix assembly registration

--- SubSearch/Properties/AssemblyInfo.cs
+++ SubSearch/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Windows;
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a0265d6e-652e-48d6-bdb9-f15984c101ba")]
