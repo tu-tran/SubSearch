38892261f03c7d3d5a74ace8c10ef40a17d9f10c
Tu Tran <hostmax@gmail.com>
2016-05-13 15:52:55 +0300

    Add support for OpenSubtitles.org

---
+++ Handlers/OpenSubtitles/OpenSubtitlesDb.cs
@@ -0,0 +1,90 @@
+﻿namespace SubSearch.Data.Handlers.OpenSubtitles
+{
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.Linq;
+
+    using OSDBnet;
+
+    using SubSearch.Data.Handlers;
+    using SubSearch.Resources;
+
+    using Language = SubSearch.Resources.Language;
+    using Subtitle = SubSearch.Data.Subtitle;
+
+    public class OpenSubtitlesDb : ISubtitleDb
+    {
+        /// <summary>
+        /// The agent.
+        /// </summary>
+        private static readonly string agent = "SubSearchTu";
+
+        /// <summary>
+        /// The client.
+        /// </summary>
+        private readonly IAnonymousClient client;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="OpenSubtitlesDb"/> class.
+        /// </summary>
+        public OpenSubtitlesDb()
+        {
+            this.client = Osdb.Login("en", agent);
+        }
+
+        /// <summary>
+        /// Downloads the specified movie file.
+        /// </summary>
+        /// <param name="releaseFile">The movie file.</param>
+        /// <param name="subtitle">The subtitle.</param>
+        /// <returns>The query result.</returns>
+        public QueryResult Download(string releaseFile, Subtitle subtitle)
+        {
+            releaseFile.DownloadSubtitle(subtitle.DownloadUrl);
+            return QueryResult.Success;
+        }
+
+        /// <summary>
+        /// Gets subtitles meta.
+        /// </summary>
+        /// <param name="releaseName">Name of the release.</param>
+        /// <param name="language"></param>
+        /// <returns>The query result.</returns>
+        public QueryResult<Subtitles> GetSubtitlesMeta(string releaseName, Language language)
+        {
+            var cultureInfo = language.GetCultureInfo();
+            var langString = cultureInfo == null ? "eng" : cultureInfo.ThreeLetterISOLanguageName;
+            var subtitles = new Subtitles();
+            subtitles.AddRange(this.client.SearchSubtitlesFromQuery(langString, releaseName).Select(this.Convert));
+            return new QueryResult<Subtitles>(QueryResult.Success, subtitles);
+        }
+
+        /// <summary>
+        /// Converts the specified subtitle.
+        /// </summary>
+        /// <param name="subtitle">The subtitle.</param>
+        /// <returns>The subtitle.</returns>
+        private Subtitle Convert(OSDBnet.Subtitle subtitle)
+        {
+            Rating rating = Rating.Neutral;
+            double ratingFigure;
+            if (double.TryParse(subtitle.Rating, NumberStyles.Any, CultureInfo.InvariantCulture, out ratingFigure))
+            {
+                if (ratingFigure < 0.1)
+                {
+                    rating = Rating.Neutral;
+                }
+                else if (ratingFigure < 5.0)
+                {
+                    rating = Rating.Negative;
+                }
+                else if (ratingFigure > 5.0)
+                {
+                    rating = Rating.Positive;
+                }
+            }
+
+            return new Subtitle(subtitle.MovieName, subtitle.MovieName, subtitle.SubTitleDownloadLink.AbsoluteUri, rating, this);
+        }
+    }
+}
---
+++ Handlers/OpenSubtitles/Properties/AssemblyInfo.cs
@@ -0,0 +1,42 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="">
+//   
+// </copyright>
+// <summary>
+//   AssemblyInfo.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SubSearch.Handlers.OpenSubtitles")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SubSearch")]
+[assembly: AssemblyProduct("SubSearch.Handlers.OpenSubtitles")]
+[assembly: AssemblyCopyright("Copyright © SubSearch 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d3c65a1a-7483-4ced-8554-4f6366baa2b6")]
+
+// Version information for an assembly consists of the following four values:
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
